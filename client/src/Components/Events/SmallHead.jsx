ee0912c35496fbc99705bcb4abb95fe31ed52d00
Ahmed Haddada <haddadaahmed9@gmail.com>
2023-12-06 14:54:53 +0100

    some edits

--- client/src/Components/Events/SmallHead.jsx
+++ client/src/Components/Events/SmallHead.jsx
@@ -3,19 +3,28 @@ import { IoIosArrowForward } from "react-icons/io";
 
 import Calendar from "react-calendar";
 import { BsCalendarDate } from "react-icons/bs";
-import { useParams } from "react-router-dom";
+import { NavLink, useLocation, useParams } from "react-router-dom";
 
 function SmallHead() {
   const [value, onChange] = useState(new Date());
   const [calander, setCalander] = useState(false);
-
-  const { place } = useParams();
+  const location = useLocation();
+  const paths = location.pathname.split("/").slice(2);
   return (
     <nav className="border-[gray] border-b-2 sticky z-30 top-16 bg-[#f7f7f7d6]  h-20 flex items-center justify-around ">
       <div className=" left-32 lg:text-lg md:left-96 flex items-center justify-center font-thin">
         <h3 className="capitalize">Category</h3>
-        <IoIosArrowForward className=" " />
-        <h3 className="capitalize">{place}</h3>
+        {paths.map((path, i) => {
+          return (
+          <div className="flex items-center" key={i}>
+              <IoIosArrowForward className=" " />
+              <NavLink to={`/events/${path}`}>
+                <h3 className="capitalize">{path}</h3>
+              </NavLink>
+          </div>
+            
+          );
+        })}
       </div>
       <BsCalendarDate
         className="text-2xl  right-10 md:right-20 cursor-pointer"
