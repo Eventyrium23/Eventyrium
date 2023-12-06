303d1494385b916ea2838bcfd2880e00594cac72
Ahmed Haddada <haddadaahmed9@gmail.com>
2023-12-06 10:30:47 +0100

    events

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,22 @@ import {
   Route,
 } from "react-router-dom";
 import { RouterProvider } from "react-router";
+//
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Pages/Home.jsx";
+//About
 import About from "./Components/Pages/About.jsx";
 import Who from "./Components/AboutLayout/Who-are.jsx";
 import Location from "./Components/AboutLayout/Location.jsx";
 import History from "./Components/AboutLayout/History.jsx";
 import Getint from "./Components/AboutLayout/Getintouch.jsx";
 import Register from "./Components/authenticateUser/Register.jsx";
+//login
 import Login from "./Components/authenticateUser/Login.jsx";
 import Verified from "./Components/authenticateUser/Verified.jsx";
-
+//events
+import LayoutEvents from "./Components/Events/LayoutEvents.jsx";
+import Places from "./Components/Events/Places.jsx";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,10 +28,14 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />}>
-            <Route index  element={<Who />} />
+            <Route index element={<Who />} />
             <Route path="location" element={<Location />} />
             <Route path="history" element={<History />} />
             <Route path="getintouch" element={<Getint />} />
+          </Route>
+
+          <Route path="/events/:places" element={<LayoutEvents />}>
+            <Route index element={<Places />} />
           </Route>
         </Route>
 
@@ -38,7 +47,7 @@ function App() {
   );
 
   return (
-    <div className="App">
+    <div className="App ">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- client/src/Components/Events/LayoutEvents.jsx
+++ client/src/Components/Events/LayoutEvents.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import SideBar from "./SideBar";
+import SideBar from "./SideBar.jsx";
 function LayoutEvents() {
   return (
-    <div className="flex">
+    <div className="flex ">
      <SideBar/>
       <Outlet />
-    </div>
+     </div>
   );
 }
 
