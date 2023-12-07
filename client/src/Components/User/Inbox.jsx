d545013ae9e143d54692b528e889a52e19cd74cd
Ahmed Haddada <haddadaahmed9@gmail.com>
2023-12-07 18:18:23 +0100

    inboxxx

--- client/src/Components/User/Inbox.jsx
+++ client/src/Components/User/Inbox.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { MyContext } from "../../MyContext.jsx";
 
 function Inbox() {
@@ -27,9 +27,44 @@ function Inbox() {
       JSON.stringify([...addedDecorations, decorationChecked])
     );
   }
-  if (packChecked.length ) {
+  if (packChecked.length) {
     window.localStorage.setItem("packs", JSON.stringify(packChecked));
   }
+
+  const Delete = (event) => {
+    const targetElement = event.target;
+    const itemName = targetElement.getAttribute("data-name");
+
+    if (addedPlaces.length && addedPlaces[0].name === itemName) {
+      const updatedPlaces = addedPlaces.slice(1);
+      window.localStorage.setItem("places", JSON.stringify(updatedPlaces));
+    } else if (
+      addedDecorations.length &&
+      addedDecorations[addedDecorations.length - 1].some(
+        (deco) => deco.name === itemName
+      )
+    ) {
+      const updatedDecorations = addedDecorations.map((decoArray) =>
+        decoArray.filter((deco) => deco.name !== itemName)
+      );
+      window.localStorage.setItem(
+        "decorations",
+        JSON.stringify(updatedDecorations)
+      );
+    } else if (
+      addedFoods.length &&
+      addedFoods[addedFoods.length - 1].some((food) => food.name === itemName)
+    ) {
+      const updatedFoods = addedFoods.map((foodArray) =>
+        foodArray.filter((food) => food.name !== itemName)
+      );
+      window.localStorage.setItem("foods", JSON.stringify(updatedFoods));
+    } else if (addedPacks.length && addedPacks[0].name === itemName) {
+      const updatedPacks = addedPacks.slice(1);
+      window.localStorage.setItem("packs", JSON.stringify(updatedPacks));
+    }
+    window.location.reload();
+  };
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg h-screen p-10">
@@ -55,7 +90,7 @@ function Inbox() {
         </thead>
         <tbody>
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            {addedPlaces.length && (
+            {addedPlaces.length ? (
               <>
                 <th
                   scope="row"
@@ -65,71 +100,85 @@ function Inbox() {
                 </th>
                 <td className="px-6 py-4">{addedPlaces[0].location}</td>
                 <td className="px-6 py-4">{addedPlaces[0].price}DT</td>
-                <td className="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                <td className="px-6 py-4 text-left">
+                  <button
+                    data-name={addedPlaces[0].name}
+                    onClick={Delete}
+                    className="text:lg text-center bg-none text-blue-600 dark:text-blue-500 hover:underline"
                   >
                     delete
-                  </a>
+                  </button>
                 </td>
               </>
+            ) : (
+              ""
             )}
           </tr>
-          {addedDecorations[addedDecorations.length - 1].map((deco, index) => (
-            <tr
-              key={index}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            >
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {deco.name}
-              </th>
+          {addedDecorations.length
+            ? addedDecorations[addedDecorations.length - 1].map(
+                (deco, index) => (
+                  <tr
+                    key={index}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {deco.name}
+                    </th>
 
-              <td className="px-6 py-4">empty</td>
-              <td className="px-6 py-4">empty</td>
-              <td className="px-6 py-4">{deco.price}DT</td>
+                    <td className="px-6 py-4">empty</td>
+                    <td className="px-6 py-4">empty</td>
+                    <td className="px-6 py-4">{deco.price}DT</td>
 
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    <td className="px-6 py-4 text-left">
+                      <button
+                        data-location={deco.name}
+                        data-name={deco.name}
+                        onClick={Delete}
+                        className="text-lg bg-none text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )
+            : ""}
+          {addedFoods.length
+            ? addedFoods[addedFoods.length - 1].map((food, index) => (
+                <tr
+                  key={index}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                 >
-                  delete
-                </a>
-              </td>
-            </tr>
-          ))}
-          {addedFoods[addedFoods.length - 1].map((food, index) => (
-            <tr
-              key={index}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            >
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {food.name}
-              </th>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {food.name}
+                  </th>
 
-              <td className="px-6 py-4">empty</td>
-              <td className="px-6 py-4">empty</td>
-              <td className="px-6 py-4">{food.price}DT</td>
+                  <td className="px-6 py-4">empty</td>
+                  <td className="px-6 py-4">empty</td>
+                  <td className="px-6 py-4">{food.price}DT</td>
 
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  delete
-                </a>
-              </td>
-            </tr>
-          ))}
+                  <td className="px-6 py-4 text-left">
+                    <button
+                      data-location={food.name}
+                      data-name={food.name}
+                      onClick={Delete}
+                      className="text-lg bg-none text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            : ""}
+
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            {addedPlaces.length && (
+            {addedPacks.length ? (
               <>
                 <th
                   scope="row"
@@ -137,17 +186,20 @@ function Inbox() {
                 >
                   {addedPacks[0].name}
                 </th>
-                <td className="px-6 py-4">{addedPlaces[0].location}</td>
-                <td className="px-6 py-4">{addedPlaces[0].price}DT</td>
-                <td className="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                <td className="px-6 py-4">{addedPacks[0].location}</td>
+                <td className="px-6 py-4">{addedPacks[0].price}DT</td>
+                <td className="px-6 py-4 text-left">
+                  <button
+                    data-name={addedPacks[0].name}
+                    onClick={Delete}
+                    className="text-lg bg-none text-blue-600 dark:text-blue-500 hover:underline"
                   >
                     delete
-                  </a>
+                  </button>
                 </td>
               </>
+            ) : (
+              ""
             )}
           </tr>
         </tbody>
